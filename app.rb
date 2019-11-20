485bab398409df282bb222fb03e612d99a5e8197
SinkLineP <popovartem505@gmail.com>
2019-11-20 19:04:49 +0200

    create DataBase in app.rb

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+def init_db
+	@db = SQLite3::Database.new 'blizary.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
